08649391688686c367f3c6ee48514dd048a464b5
Kate Huang <123223306+Lilsofie@users.noreply.github.com>
2024-08-15 14:23:44 +0800

    Update IPinfo.py

--- IPinfo.py
+++ IPinfo.py
@@ -8,20 +8,24 @@ def getIPgeo(IP,apitoken):
     geo_url = base_url + IP + "?token=" + apitoken
     geo_response = requests.get(geo_url)
     if geo_response.status_code == 200:
-       geo_response = geo_response.json()
-       if geo_response.get("hostname") != None:
+        geo_response = geo_response.json()
+        # print(geo_response)
+        if geo_response.get("hostname") != None:
             hostname = geo_response["hostname"]
             result["Hostname"] = hostname
-       else:
-           result["Hostname"] = 'None'
-       result["City"] = geo_response["city"]
-       result["Region"] = geo_response["region"]
-       result["Country"] = geo_response["country"]
-       result["Org"] =  geo_response["org"]
-       location = geo_response["loc"].split(",")
-       result["latitude"] = location[0]
-       result["longitude"] = location[1]       
-
+        else:
+            result["Hostname"] = 'None'
+        if geo_response.get("org") != None:
+            result["Org"] = geo_response["org"]
+        else:
+            result["Org"] = 'None'
+        result["City"] = geo_response["city"]
+        result["Region"] = geo_response["region"]
+        result["Country"] = geo_response["country"]
+        location = geo_response["loc"].split(",")
+        result["latitude"] = location[0]
+        result["longitude"] = location[1]       
+            
     else:
         print(f"API call failed with status code: {geo_response.status_code}")
         result = geo_response.text
